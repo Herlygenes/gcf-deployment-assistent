5b2959bad5b2fd1f3db14f0644441ba93b8782fd
Herlygenes Pinto <herlygenes.pinto@gmail.com>
2022-12-21 14:42:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputBoxElement = document.getElementById("input");
+inputBoxElement.addEventListener("change", () => {
+  let outputCommand = "firebase deploy --only ";
+  const arrayOfLines = inputBoxElement.value.split("\n");
+  arrayOfLines.shift();
+  arrayOfLines.pop();
+  for (const [i,f] of arrayOfLines.entries()) {
+    const fName = f.split('(')[0]
+    outputCommand = `${outputCommand}functions:${fName.trim()}${i < arrayOfLines.length -1 && ","}`;
+  }
+  document.querySelector('#output').innerHTML = outputCommand;
+  console.log(outputCommand);
+});
